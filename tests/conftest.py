c80cbaa2e514e3979ec49af0171608ccbfc4e5f9
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-09-23 13:03:34 +0200

    añadido tets

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+
+import pytest
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+@pytest.fixture
+def app():
+
+    ''' tempfile mkstemp crea y abre un archivo temporal. En create_app se 
+    sobreescribe la ruta a la base de datos temporalmente para utilizar la db
+    de prueba.
+    '''
+    db_fb, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fb)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
