4dc845e9350b8065fc2ade0f7cfa6fb6c01854f4
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-09-26 11:22:23 +0200

    acabado código tests

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -15,6 +15,8 @@ def test_index(client, auth):
     assert b'test\nbody' in response.data
     assert b'href="/1/update"' in response.data
 
+# comprobar que el usuario debe estar logueado para acceder a las
+# views de create, update, delete.
 @pytest.mark.parametrize('path', (
     '/create',
     '/1/update',
@@ -23,4 +25,72 @@ def test_index(client, auth):
 def test_login_required(client, path):
     response = client.post(path)
     assert response.headers["Location"] == "/auth/login"
+    
+def test_author_required(app, client, auth):
+    # cambiando el autor del post a otro usuario
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
+        db.commit()
 
+    auth.login()
+    # usuario actual no puede modificar el post de otro usuario
+    assert client.post('/1/update').status_code == 403 # status 403 forbidden
+    assert client.post('/1/delete').status_code == 403
+    # el usuario actual no ve el enlace para editar
+    assert b'href="/1/update"' not in client.get('/').data
+
+# comprobar que un post que no existe devuelve not found cuando le hacemos 
+# delete o update
+@pytest.mark.parametrize('path', (
+    '/2/update',
+    '/2/delete',
+))
+def test_exists_required(client, auth, path):
+    auth.login()
+    assert client.post(path).status_code == 404 # status 404 not found
+
+# obtener un status 200 en get y post si enviamos datos válidos. Create y
+# update deberían funcionar como esperado, creando entrada y modificando 
+# entrada
+
+def test_create(client, auth, app):
+    auth.login()
+    assert client.get('/create').status_code == 200
+    client.post('/create', data={'title': 'create', 'body': ''})
+
+    with app.app_context():
+        db = get_db()
+        count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
+        assert count == 2
+
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get('/1/update').status_code == 200
+    client.post('/1/update', data={'title':'update', 'body': ''})
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post['title'] == 'update'
+
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={'title':'', 'body':''})
+    assert b'Title is required.' in response.data
+
+# si delete funciona bien, debe redirigir al index y el post
+# eliminado no debería aparecer
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post('/1/delete')
+    assert response.headers["Location"] == "/"
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
+        assert post is None
